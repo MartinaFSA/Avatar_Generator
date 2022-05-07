b9f30e7bc2a36e1abd8586f0dcf5d86eb55e8a76
Martina <martinafsa8>
2022-05-06 22:13:47 -0300

    Responsive, frontend y mejoras de funcionalidad

--- src/App.js
+++ src/App.js
@@ -3,36 +3,21 @@ import './styles.css';
 import React, { useState } from 'react';
 import html2canvas from 'html2canvas';
 
-function Cambioalor(){
-  const [cont, setCont]= useState(0); //useState (valor en el que incio cont) y setCont (manipulador de contenido)
-
-  const sumaClick = () => {
-    setCont(cont + 1)
-  }
-
-  return (
-    <div className='texto_centrado'>
-      <h1>Sumar valores</h1>
-      <p>{cont}</p>
-      <button onClick={sumaClick}>Agregar</button>
-      <hr></hr>
-    </div>
-  )
-}
-
 function PersonajeExportar() {
   /* Asignación con useState */
-  const [ojos, setOjos] = useState(1);
-  const [boca, setBoca] = useState(3);
-  const [accesorio, setAccesorio] = useState(1);
-  const [piel, setPiel] = useState(1);
-  const [fondo, setFondo] = useState(1);
+  let [ojos, setOjos] = useState(1);
+  let [boca, setBoca] = useState(3);
+  let [accesorio, setAccesorio] = useState(1);
+  let [piel, setPiel] = useState(1);
+  let [fondo, setFondo] = useState(1);
+  let [ropa, setRopa] = useState(1);
   /*Mínimo 4 por array*/
-  const arrOjos = [1,0,2,3];
-  const arrAccesorio = [1,2,3,4];
-  const arrBoca = [1,2,3,4];
+  const arrOjos = [1,0];
+  const arrAccesorio = [1,2];
+  const arrBoca = [2,3];
   const arrPiel = [1,2,3,4];
   const arrFondo = [1,2,3,4,5];
+  const arrRopa = [1];
   
   /* Exportar archivo */
   const archivoExportado = function(i){
@@ -44,7 +29,7 @@ function PersonajeExportar() {
       link.click();
     });
   }
-
+  
   /* event listeners de los useState */
   const pielSeleccionada = function(e){
    setPiel(e.target.id);
@@ -56,6 +41,8 @@ function PersonajeExportar() {
     setAccesorio(e.target.id);
   }; const fondoSeleccionado = function(e){
     setFondo(e.target.id);
+  }; const ropaSeleccionado = function(e){
+    setRopa(e.target.id);
   };
   
   /*Variables conseguidas por ID*/
@@ -64,43 +51,81 @@ function PersonajeExportar() {
   const opciones_accesorio = document.getElementById("rasgos_accesorio");
   const opciones_ojos = document.getElementById("rasgos_ojos");
   const opciones_fondo = document.getElementById("rasgos_fondo");
+  const opciones_ropa = document.getElementById("rasgos_ropa");
   
   function displayOjos(){
     opciones_boca.style.display = "none";
     opciones_piel.style.display = "none"; 
     opciones_fondo.style.display = "none";
     opciones_accesorio.style.display = "none";
+    opciones_ropa.style.display = "none";
     opciones_ojos.style.display = "block";
   };  function displayAccesorio(){    
     opciones_boca.style.display = "none";    
     opciones_piel.style.display = "none";
     opciones_fondo.style.display = "none";
     opciones_ojos.style.display = "none";
+    opciones_ropa.style.display = "none";
     opciones_accesorio.style.display = "block";
   };  function displayBoca(){ 
     opciones_piel.style.display = "none";
     opciones_accesorio.style.display = "none";
     opciones_ojos.style.display = "none";
     opciones_fondo.style.display = "none";
+    opciones_ropa.style.display = "none";
     opciones_boca.style.display = "block";   
   };  function displayPiel(){    
     opciones_boca.style.display = "none";
     opciones_accesorio.style.display = "none";
     opciones_ojos.style.display = "none";
     opciones_fondo.style.display = "none";
+    opciones_ropa.style.display = "none";
     opciones_piel.style.display = "block";
   };  function displayFondo(){    
     opciones_boca.style.display = "none";
     opciones_accesorio.style.display = "none";
     opciones_ojos.style.display = "none";
     opciones_piel.style.display = "none";
+    opciones_ropa.style.display = "none";
     opciones_fondo.style.display = "block";
+  };  function displayVestimenta(){    
+    opciones_boca.style.display = "none";
+    opciones_accesorio.style.display = "none";
+    opciones_ojos.style.display = "none";
+    opciones_piel.style.display = "none";
+    opciones_fondo.style.display = "none";
+    opciones_ropa.style.display = "block";
   };
+
+  /*Random avatar*/
+  function randomFondo(e) {
+    setFondo(e);
+  }; function randomOjo(e) {
+    setOjos(e);
+  }; function randomPiel(e) {
+    setPiel(e);
+  }; function randomBoca(e) {
+    setBoca(e);
+  };
+  function randomCharacter() {
+    let random_ojo = Math.floor(Math.random() * arrOjos.length);
+    let random_boca = Math.floor(Math.random() * arrBoca.length);
+    let random_piel = Math.floor(Math.random() * arrPiel.length);
+    let random_fondo = Math.floor(Math.random() * arrFondo.length);
+    piel = random_piel + 1;
+    ojos = random_ojo;
+    boca = random_boca + 1;
+    fondo = random_fondo + 1;
+    randomFondo(fondo);
+    randomOjo(ojos);
+    randomBoca(boca);
+    randomPiel(piel);
+  }
 
   return (
     <div>
       <section id='header'>
-        <button className='icon'><img src='/img/icons/dice.png' alt='generar imagen al azar'></img></button>
+        <button className='icon' onClick={randomCharacter}><img src='/img/icons/dice.png' alt='generar imagen al azar'></img></button>
         <p id='nombre_app'>Avatar generator</p>
         <button className='icon' onClick={archivoExportado}><img src='/img/icons/download.png' alt='descargar imagen'></img></button>
       </section>
@@ -117,24 +142,30 @@ function PersonajeExportar() {
               <img src={"/img/accesorio/" + accesorio + ".png"} className='img_personaje'/>
               <img src={"/img/base/ropaTraje.png"} alt=" " className='img_personaje'/>
             </div>
-            <button onClick={archivoExportado}>Exportar personaje</button>
+            <div>
+              <button onClick={archivoExportado}>Exportar personaje</button>
+              <button onClick={randomCharacter}><img src='/img/icons/dice.png' alt='generar imagen al azar'></img></button>
+            </div>
           </figure>
           <article>
             <ul className='scroll_horizontal'>
               <li> 
-                <button onClick={displayPiel}>Piel</button>
+                <button onClick={displayPiel}><img src='./img/icons/piel.png'></img>Piel</button>
               </li>
               <li>
-                <button onClick={displayBoca}>Boca</button>
+                <button onClick={displayBoca}><img src='./img/icons/labio.png'></img>Boca</button>
               </li>
               <li>
-                <button onClick={displayOjos}>Ojos</button>
+                <button onClick={displayOjos}><img src='./img/icons/ojo.png'></img>Ojos</button>
               </li>
               <li>
-                <button onClick={displayAccesorio}>Accesorios</button>
+                <button onClick={displayAccesorio}><img src='./img/icons/accesorio.png'></img>Accesorios</button>
               </li>
               <li>
-                <button onClick={displayFondo}>Fondo</button>
+                <button onClick={displayFondo}><img src='./img/icons/background.png'></img>Fondo</button>
+              </li>
+              <li>
+                <button onClick={displayVestimenta}><img src='./img/icons/ropa.png'></img>Vestimenta</button>
               </li>
             </ul>
             <ul className='rasgos scroll_horizontal' id='rasgos_piel'>
@@ -162,6 +193,11 @@ function PersonajeExportar() {
                   <li key={fondo.toString()}> <button onClick={fondoSeleccionado} id={fondo}> <img src={"/img/miniaturaFondos/" + fondo + ".png"} alt="fondo" id={fondo}></img></button></li>  
                 ))}
             </ul>
+            <ul className='rasgos scroll_horizontal' id='rasgos_ropa'>
+              {arrRopa.map((ropa) => (
+                  <li key={ropa.toString()}> <button onClick={ropaSeleccionado} id={ropa}> <img src={"/img/miniaturaVestimenta/" + ropa + ".png"} alt="ropa" id={ropa}></img></button></li>  
+                ))}
+            </ul>
         </article>
       </section>
     </div>
@@ -171,7 +207,6 @@ function PersonajeExportar() {
 function App() {
   return (
     <div className="App">
-      <Cambioalor />
       <PersonajeExportar />
     </div>
   );
