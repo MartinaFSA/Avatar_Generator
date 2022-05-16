4e4e9206de7850e1a12fe833b9d98417aee4a638
Martina <martinafsa8>
2022-05-16 11:42:21 -0300

    Mejoras frontend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import './styles.css';
 import React, { useState } from 'react';
 import html2canvas from 'html2canvas';
@@ -23,7 +22,7 @@ function PersonajeExportar() {
   
   /* Exportar archivo */
   const archivoExportado = function(i){
-    html2canvas(document.querySelector("#avatar_exportado")).then(canvas => {
+    html2canvas(document.querySelector("#todo")).then(canvas => {
       let img = canvas.toDataURL("img/png");
       let link = document.createElement("a"); /*Elemento que descargaremos*/
       link.download = "Avatar_Generator.png"; /*el nombre que tendrá el archivo descargado*/
@@ -65,7 +64,7 @@ function PersonajeExportar() {
     opciones_pelo.style.display = "none";
     opciones_accesorio.style.display = "none";
     opciones_ropa.style.display = "none";
-    opciones_ojos.style.display = "block";
+    opciones_ojos.style.display = "flex";
   };  function displayAccesorio(){    
     opciones_boca.style.display = "none";    
     opciones_piel.style.display = "none";
@@ -73,7 +72,7 @@ function PersonajeExportar() {
     opciones_fondo.style.display = "none";
     opciones_ojos.style.display = "none";
     opciones_ropa.style.display = "none";
-    opciones_accesorio.style.display = "block";
+    opciones_accesorio.style.display = "flex";
   };  function displayBoca(){ 
     opciones_piel.style.display = "none";
     opciones_accesorio.style.display = "none";
@@ -81,7 +80,7 @@ function PersonajeExportar() {
     opciones_fondo.style.display = "none";
     opciones_pelo.style.display = "none";
     opciones_ropa.style.display = "none";
-    opciones_boca.style.display = "block";   
+    opciones_boca.style.display = "flex";   
   };  function displayPiel(){    
     opciones_boca.style.display = "none";
     opciones_accesorio.style.display = "none";
@@ -89,7 +88,7 @@ function PersonajeExportar() {
     opciones_ojos.style.display = "none";
     opciones_fondo.style.display = "none";
     opciones_ropa.style.display = "none";
-    opciones_piel.style.display = "block";
+    opciones_piel.style.display = "flex";
   };  function displayFondo(){    
     opciones_boca.style.display = "none";
     opciones_accesorio.style.display = "none";
@@ -97,7 +96,7 @@ function PersonajeExportar() {
     opciones_pelo.style.display = "none";
     opciones_piel.style.display = "none";
     opciones_ropa.style.display = "none";
-    opciones_fondo.style.display = "block";
+    opciones_fondo.style.display = "flex";
   };  function displayVestimenta(){    
     opciones_boca.style.display = "none";
     opciones_accesorio.style.display = "none";
@@ -105,7 +104,7 @@ function PersonajeExportar() {
     opciones_piel.style.display = "none";
     opciones_pelo.style.display = "none";
     opciones_fondo.style.display = "none";
-    opciones_ropa.style.display = "block";
+    opciones_ropa.style.display = "flex";
   };  function displayPelo(){    
     opciones_boca.style.display = "none";
     opciones_accesorio.style.display = "none";
@@ -113,7 +112,7 @@ function PersonajeExportar() {
     opciones_piel.style.display = "none";
     opciones_fondo.style.display = "none";
     opciones_ropa.style.display = "none";
-    opciones_pelo.style.display = "block";
+    opciones_pelo.style.display = "flex";
   };
 
   /*Random avatar*/
@@ -147,24 +146,24 @@ function PersonajeExportar() {
   }
 
   return (
-    <div>
+    <div id='todo'>
       <section id='header'>
-        <button className='icon' onClick={randomCharacter}><img src='/img/icons/dice.png' alt='generar imagen al azar'></img></button>
+        <button className='icon' onClick={randomCharacter}><img src='../img/icons/dice.png' alt='generar imagen al azar'></img></button>
         <p id='nombre_app'>Avatar generator</p>
         <button className='icon' onClick={archivoExportado}><img src='/img/icons/download.png' alt='descargar imagen'></img></button>
       </section>
       <section className='selectorRasgos'>
           <figure id="exportar">
             <div id='avatar_exportado'>
-              <img src={"/img/fondo/" + fondo + ".png"} className='img_personaje'  />
-              <img src={"/img/piel/" + piel + ".png"} alt=" " className='img_personaje'  />
-              <img src={"/img/base/baseOjosNariz.png"} className='img_personaje'  />
-              <img src={"/img/ojos/" + ojos + ".png"} alt=" " className='img_personaje'  />
-              <img src={"/img/base/brilloOjos.png"} className='img_personaje'  />
-              <img src={"/img/boca/" + boca + ".png"} alt=" " className='img_personaje'  />
-              <img src={"/img/accesorio/" + accesorio + ".png"} className='img_personaje'  />
-              <img src={"/img/base/ropaTraje.png"} alt=" " className='img_personaje'  />
-              <img src={"/img/pelo/" + pelo + ".png"} alt=" " className='img_personaje'  />
+              <img src={ "./img/fondo/" + fondo + ".png"} className='img_personaje'  />
+              <img src={ "./img/piel/" + piel + ".png"} alt=" " className='img_personaje'  />
+              <img src={ "./img/base/baseOjosNariz.png"} className='img_personaje'  />
+              <img src={ "./img/ojos/" + ojos + ".png"} alt=" " className='img_personaje'  />
+              <img src={ "./img/base/brilloOjos.png"} className='img_personaje'  />
+              <img src={ "./img/boca/" + boca + ".png"} alt=" " className='img_personaje'  />
+              <img src={ "./img/accesorio/" + accesorio + ".png"} className='img_personaje'  />
+              <img src={ "./img/base/ropaTraje.png"} alt=" " className='img_personaje'  />
+              <img src={ "./img/pelo/" + pelo + ".png"} alt=" " className='img_personaje'  />
             </div>
             <div>
               <button onClick={archivoExportado}>Exportar personaje</button>
@@ -187,47 +186,47 @@ function PersonajeExportar() {
               </li>
               <li>
                 <button onClick={displayFondo}><img src='./img/icons/background.png'></img>Fondo</button>
-              </li> {/*
-              <li>
+              </li> 
+              {/*<li>
                 <button onClick={displayVestimenta}><img src='./img/icons/ropa.png'></img>Vestimenta</button>
-  </li>*/}
+              </li>*/}
               <li>
                 <button onClick={displayPelo}><img src='./img/icons/pelo.png'></img>Pelo</button>
               </li>
             </ul>
             <ul className='rasgos scroll_horizontal' id='rasgos_piel'>
               {arrPiel.map((piel) => (
-                  <li key={piel.toString()}> <button onClick={pielSeleccionada} id={piel}> <img src={"/img/miniaturaPiel/" + piel + ".png"} alt="color de piel" id={piel}></img></button></li>  
+                  <li key={piel.toString()}> <button onClick={pielSeleccionada} id={piel}> <img src={ "./img/miniaturaPiel/" + piel + ".png"} alt="color de piel" id={piel}></img></button></li>  
                 ))}
             </ul>
             <ul className='rasgos scroll_horizontal' id='rasgos_boca'>
               {arrBoca.map((boca) => (
-                  <li key={boca.toString()}> <button onClick={bocaSeleccionada} id={boca}> <img src={"/img/miniaturaLabios/" + boca + ".png"} alt="color de labios" id={boca}></img></button></li>  
+                  <li key={boca.toString()}> <button onClick={bocaSeleccionada} id={boca}> <img src={ "./img/miniaturaLabios/" + boca + ".png"} alt="color de labios" id={boca}></img></button></li>  
                 ))}
             </ul>
             <ul className='rasgos scroll_horizontal' id='rasgos_accesorio'>
               {arrAccesorio.map((accesorio) => (
-                  <li key={accesorio.toString()}> <button onClick={accesorioSeleccionada} id={accesorio}> <img src={"/img/miniaturaAccesorio/" + accesorio + ".png"} id={accesorio}></img></button></li>  
+                  <li key={accesorio.toString()}> <button onClick={accesorioSeleccionada} id={accesorio}> <img src={ "./img/miniaturaAccesorio/" + accesorio + ".png"} id={accesorio}></img></button></li>  
                 ))}
             </ul>
             <ul className='rasgos scroll_horizontal' id='rasgos_ojos'>
               {arrOjos.map((ojo) => (
-                  <li key={ojo.toString()}> <button onClick={ojosSeleccionados} id={ojo}> <img src={"/img/miniaturaOjos/" + ojo + ".png"} alt="color de ojos" id={ojo}></img></button></li>  
+                  <li key={ojo.toString()}> <button onClick={ojosSeleccionados} id={ojo}> <img src={ "./img/miniaturaOjos/" + ojo + ".png"} alt="color de ojos" id={ojo}></img></button></li>  
                 ))}
             </ul>
             <ul className='rasgos scroll_horizontal' id='rasgos_fondo'>
               {arrFondo.map((fondo) => (
-                  <li key={fondo.toString()}> <button onClick={fondoSeleccionado} id={fondo}> <img src={"/img/miniaturaFondos/" + fondo + ".png"} alt="fondo" id={fondo}></img></button></li>  
+                  <li key={fondo.toString()}> <button onClick={fondoSeleccionado} id={fondo}> <img src={ "./img/miniaturaFondos/" + fondo + ".png"} alt="fondo" id={fondo}></img></button></li>  
                 ))}
             </ul>
             <ul className='rasgos scroll_horizontal' id='rasgos_ropa'>
               {arrRopa.map((ropa) => (
-                  <li key={ropa.toString()}> <button onClick={ropaSeleccionado} id={ropa}> <img src={"/img/miniaturaVestimenta/" + ropa + ".png"} alt="ropa" id={ropa}></img></button></li>  
+                  <li key={ropa.toString()}> <button onClick={ropaSeleccionado} id={ropa}> <img src={ "./img/miniaturaVestimenta/" + ropa + ".png"} alt="ropa" id={ropa}></img></button></li>  
                 ))}
             </ul>
             <ul className='rasgos scroll_horizontal' id='rasgos_pelo'>
               {arrPelo.map((pelo) => (
-                  <li key={pelo.toString()}> <button onClick={peloSeleccionado} id={pelo}> <img src={"/img/miniaturaPelo/" + pelo + ".png"} alt="pelo" id={pelo}></img></button></li>  
+                  <li key={pelo.toString()}> <button onClick={peloSeleccionado} id={pelo}> <img src={ "./img/miniaturaPelo/" + pelo + ".png"} alt="pelo" id={pelo}></img></button></li>  
                 ))}
             </ul>
         </article>
